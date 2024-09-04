dbddc8d8ea5e224c6c9f3cff47e854b4bba62a0e
izzadewanata <izza.career@gmail.com>
2024-09-05 00:04:16 +1000

    version 2

--- README.md
+++ README.md
@@ -3,3 +3,16 @@ Push code to GitHub - VS Code command line
     git add .
     git commit -m "message here"
     git push
+
+
+    Raw note: 
+get div element into JavaScript.
+create innerHTML for h1.
+create array for words.
+create let variable for Words index array.
+create let variable for Character index array.
+create a function, add incremental of char index and include h1. Call the function.
+attach slice method, for animation. (0,charindex). Increment char index. write setTimeout method (function, time).
+create condition; if char index is equal to index length, increment words index and increment char index.
+create condition; if it finished run through career index, set career index back to 0.
+create condition; if the first char is A/I/U/E/O, change "a" to "an".
--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const containerEl = document.querySelector(".container")
 
 const wordArray = ["Hello there!", "My name is Izza Sinatrya", "I'm a Software Developer"]
-
 let wordIndex = 0
 let charIndex = 0
 
@@ -10,23 +9,23 @@ updateText()
 
 function updateText() {
     charIndex++     //must place this code on the first line (*error: last word)
+    
+    //condition; if it finish run through career index, set career index back to 0 (loop).
+    if(wordIndex === wordArray.length){
+        wordIndex = 0
+    }
+
     const actionWord = wordArray[wordIndex].slice(0,charIndex)
     
     //print into HTML
     containerEl.innerHTML =`
-    <h1>- ${actionWord} -</h1>
-`
+    <h1>- ${actionWord} -</h1>`
     //condition; change the Word array index if it finish rendered the sentence. 
     if(charIndex === wordArray[wordIndex].length){
         setTimeout(() => {
             wordIndex++
             charIndex = 0
         }, 400);
-    }
-
-    //condition; if it finish run through career index, set career index back to 0 (loop).
-    if(wordIndex === wordArray.length){
-        wordIndex = 0
     }
 
     // method to call a function after a certain period.
